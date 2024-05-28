cd63a9c18b07c5a81c78e8ab5590598f37e11563
tho888 <105895875+tho888@users.noreply.github.com>
2024-05-28 16:30:54 +1200

    testing warping

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 let sourceImg = null;
 let maskImg = null;
-let renderCounter = 0;
 
 // change these three lines as appropiate
 let sourceFile = "input_1.jpg";
@@ -23,23 +22,42 @@ function setup() {
   maskImg.loadPixels();
 }
 
+let x_stop = 3000;
+let y_stop = 2000;
+let offset = 20;
+let renderCounter = 0;
+
 function draw() {
-  for (let i = 0; i < 4000; i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-    if (mask[0] > 128) {
-      let pointSize = 11;
-      ellipse(x, y, pointSize, pointSize);
-    } else {
-      let pointSize = 3;
-      rect(x, y, pointSize, pointSize);
+  angleMode(DEGREES);
+  let linesToDraw = 40;
+
+  for (let j = renderCounter; j < renderCounter + linesToDraw && j < y_stop; j++)
+    for (let i = 0; i < x_stop; i++) {
+      // let x = floor(random(sourceImg.width));
+      // let y = floor(random(sourceImg.height));
+      // let pix = sourceImg.get(x, y);
+      let mask = maskImg.get(i, j);
+      // fill(pix);
+
+      if (mask[0] < 128) {
+        // let pointSize = 11;
+        // ellipse(x, y, pointSize, pointSize);
+        pix = sourceImg.get(i, j);
+      } else {
+        // let pointSize = 3;
+        // rect(x, y, pointSize, pointSize);
+        let wave = sin(j * 3);
+        let slip = map(wave, -1, 1, -offset, offset);
+        pix = sourceImg.get(i + slip, j);
+      }
+
+      set(i, j, pix);
     }
-  }
-  renderCounter = renderCounter + 1;
-  if (renderCounter > 10) {
+
+  renderCounter = renderCounter + linesToDraw;
+  updatePixels();
+
+  if (renderCounter > y_stop) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
