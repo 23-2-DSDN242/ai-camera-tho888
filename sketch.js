220a243c06f12263bbe416b419bb1c43af41af61
tho888 <105895875+tho888@users.noreply.github.com>
2024-05-28 14:56:11 +1200

    remove image

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-let sourceImg=null;
-let maskImg=null;
-let renderCounter=0;
+let sourceImg = null;
+let maskImg = null;
+let renderCounter = 0;
 
 // change these three lines as appropiate
 let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
+let maskFile = "mask_1.png";
 let outputFile = "output_1.png";
 
 function preload() {
@@ -12,7 +12,7 @@ function preload() {
   maskImg = loadImage(maskFile);
 }
 
-function setup () {
+function setup() {
   let main_canvas = createCanvas(3000, 2000);
   main_canvas.parent('canvasContainer');
 
@@ -23,24 +23,23 @@ function setup () {
   maskImg.loadPixels();
 }
 
-function draw () {
-  for(let i=0;i<4000;i++) {
+function draw() {
+  for (let i = 0; i < 4000; i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
-    if(mask[0] > 128) {
+    if (mask[0] > 128) {
       let pointSize = 11;
       ellipse(x, y, pointSize, pointSize);
-    }
-    else {
+    } else {
       let pointSize = 3;
-      rect(x, y, pointSize, pointSize);    
+      rect(x, y, pointSize, pointSize);
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if (renderCounter > 10) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
