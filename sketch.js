48de349e10e9e2de07a81757a464f682ec2ebe32
tho888 <105895875+tho888@users.noreply.github.com>
2024-05-31 13:06:55 +1200

    Completed main code

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 let sourceImg = null;
 let maskImg = null;
+let renderCounter = 0;
+let curLayer = 0;
 
 // change these three lines as appropiate
 let sourceFile = "input_1.jpg";
@@ -17,51 +19,65 @@ function setup() {
 
   imageMode(CENTER);
   noStroke();
-  background(32, 32, 32);
+  background(246);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 let x_stop = 3000;
 let y_stop = 2000;
-let offset = 20;
-let renderCounter = 0;
+let offset = 17;
 
 function draw() {
-  angleMode(DEGREES);
-  let linesToDraw = 40;
 
-  for (let j = renderCounter; j < renderCounter + linesToDraw && j < y_stop; j++)
-    for (let i = 0; i < x_stop; i++) {
-      // let x = floor(random(sourceImg.width));
-      // let y = floor(random(sourceImg.height));
-      // let pix = sourceImg.get(x, y);
-      let mask = maskImg.get(i, j);
-      // fill(pix);
+  if (curLayer == 0) {
 
-      if (mask[0] < 128) {
-        // let pointSize = 11;
-        // ellipse(x, y, pointSize, pointSize);
-        pix = sourceImg.get(i, j);
-      } else {
-        // let pointSize = 3;
-        // rect(x, y, pointSize, pointSize);
-        let wave = sin(j * 3);
-        let slip = map(wave, -1, 1, -offset, offset);
-        pix = sourceImg.get(i + slip, j);
+    angleMode(DEGREES);
+    let linesToDraw = 100;
+
+    for (let j = renderCounter; j < renderCounter + linesToDraw && j < y_stop; j++)
+      for (let i = 0; i < x_stop; i++) {
+        let mask = maskImg.get(i, j);
+
+        if (mask[0] < 128) {
+          pix = sourceImg.get(i, j);
+        } else {
+          let wave = sin(j * 3);
+          let slip = map(wave, -1, 1, -offset, offset);
+          pix = sourceImg.get(i + slip, j);
+        }
+
+        set(i, j, pix);
       }
 
-      set(i, j, pix);
+    renderCounter = renderCounter + linesToDraw;
+    updatePixels();
+
+  } else {
+
+    for (let i = 0; i < 20; i++) {
+      let x = floor(random(sourceImg.width));
+      let y = floor(random(sourceImg.height));
+      let pix = sourceImg.get(x, y);
+      let mask = maskImg.get(x, y);
+      fill(pix);
+
+      if (mask[0] < 128) {
+        let pointSize = 39;
+        ellipse(x, y, pointSize, pointSize);
+      }
     }
+    renderCounter = renderCounter + 1;
+  }
 
-  renderCounter = renderCounter + linesToDraw;
-  updatePixels();
-
-  if (renderCounter > y_stop) {
-    console.log("Done!")
+  if (curLayer == 0 && renderCounter > 2000) {
+    curLayer = 1;
+    renderCounter = 0;
+  } else if (curLayer == 1 && renderCounter > 500) {
+    console.log("Done!");
     noLoop();
     // uncomment this to save the result
-    // saveArtworkImage(outputFile);
+    saveArtworkImage(outputFile);
   }
 }
 
